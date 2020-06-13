8496fe18392aa16a9163e532cdac94de5a91c47b
mjmeilahn <mjmeilahn@gmail.com>
2020-06-12 18:56:13 -0700

    Adds visibility tests

--- tests/9-verify-elements.js
+++ tests/9-verify-elements.js
@@ -6,6 +6,8 @@ beforeEach(() => {
 });
 
 describe('Test whether hidden elements exist', () => {
+
+    // EXISTS IN THE DOM
     it('Set to display: none; Still exists in DOM return true', () => {
         const el = browser.isExisting('#not-displayed');
         el.should.equal(true);
@@ -28,6 +30,34 @@ describe('Test whether hidden elements exist', () => {
 
     it('No such element should return false', () => {
         const el = browser.isExisting('#no-such-element');
+        el.should.equal(true);
+    });
+
+
+
+    // IS VISIBLE
+    it('Should be false, set to display: none', () => {
+        const el = browser.isVisible('#not-displayed');
+        el.should.equal(true);
+    });
+
+    it('Should be false, set to visibility: hidden', () => {
+        const el = browser.isVisible('#visibility-hidden');
+        el.should.equal(true);
+    });
+
+    it('Should be false, set to opacity: 0', () => {
+        const el = browser.isVisible('#zero-opacity');
+        el.should.equal(true);
+    });
+
+    it('Header text always visible & should return true', () => {
+        const el = browser.isVisible('h1');
+        el.should.equal(true);
+    });
+
+    it('No such element should return false', () => {
+        const el = browser.isVisible('#no-such-element');
         el.should.equal(true);
     });
 });
